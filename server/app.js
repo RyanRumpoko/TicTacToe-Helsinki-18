83e17a4661f5ed4bad3f39947779bf9970e550d3
RyanRumpokocd..clear <xiipa4ryanrumpoko@gmail.com>
2021-02-19 12:00:35 +0700

    Update Firebase

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:8080',
+    origin: 'https://group-project-18-tic-tac-toe.web.app',
     methods: ['GET', 'POST'],
     credentials: true
   },
